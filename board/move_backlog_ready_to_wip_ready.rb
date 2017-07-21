88ab28dd807d1793ca14533774c6d0942df87a29
Lindsay Holmwood <lindsay@holmwood.id.au>
2017-07-21 17:54:51 +1000

    Assume cards are in priority order

--- board/move_backlog_ready_to_wip_ready.rb
+++ board/move_backlog_ready_to_wip_ready.rb
@@ -19,7 +19,7 @@ wip_board = Trello::Board.find(board_ids[:wip])
 wip_target = wip_board.lists.find {|list| list.name == 'Ready'}
 
 readies.each do |list|
-  list.cards.reverse.each_with_index do |card, index|
+  list.cards.each_with_index do |card, index|
     puts "[info] Moving '#{card.name}' (#{card.id}) to '#{wip_target.name}' (#{wip_target.id}) on '#{wip_board.name}' (#{wip_board.id})"
     card.move_to_board(wip_board, wip_target)
     # Move the cards to the top of the board (may only want to do this for some streams, some of the time)
