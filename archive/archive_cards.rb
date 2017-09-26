9582155ed3b6ea1cd49fee2647d6bce38af0e7af
Lindsay Holmwood <lindsay@holmwood.id.au>
2017-09-26 17:59:02 +1000

    Fix style per rubocop

--- archive/archive_cards.rb
+++ archive/archive_cards.rb
@@ -1,4 +1,4 @@
-#!/usr/bin/env ruby
+# frozen_string_literal: true
 
 require 'scraperwiki'
 require 'trello'
@@ -6,20 +6,30 @@ require 'pry'
 require 'dotenv'
 Dotenv.load
 
+Trello.configure do |config|
+  config.developer_public_key = ENV['TRELLO_DEVELOPER_PUBLIC_KEY']
+  config.member_token = ENV['TRELLO_MEMBER_TOKEN']
+end
+
 def existing_record_ids(table: 'data', id: 'id')
   @cached ||= {}
-  if @cached[table]
-    return @cached[table]
-  else
-    @cached[table] = ScraperWiki.select("#{id} from #{table}").map {|r| r[id]}
-  end
+  return @cached[table] if @cached[table]
+  @cached[table] = ScraperWiki.select("#{id} from #{table}").map { |r| r[id] }
 rescue SqliteMagic::NoSuchTable
   []
 end
 
-Trello.configure do |config|
-  config.developer_public_key = ENV['TRELLO_DEVELOPER_PUBLIC_KEY']
-  config.member_token = ENV['TRELLO_MEMBER_TOKEN']
+def actions_from_cards(cards)
+  cards.map do |card|
+    a = JSON.parse(card.actions.to_json)
+    a.map do |attr|
+      {
+        'id'      => attr['id'],
+        'card_id' => card.id,
+        'json'    => attr.merge('card_id' => card.id).to_json
+      }
+    end
+  end.flatten!
 end
 
 def cards_and_actions
@@ -27,34 +37,38 @@ def cards_and_actions
   board = Trello::Board.find(board_id)
 
   cards = board.cards
+  actions = actions_from_cards(cards)
+  cards.map! { |c| { 'id': c.id, 'json': c.to_json } }
 
-  actions = cards.map { |card|
-    a = JSON.parse(card.actions.to_json)
-    a.map {|a|
-      {
-        'id'      => a['id'],
-        'card_id' => card.id,
-        'json'    => a.merge('card_id' => card.id).to_json
-      }
-    }
-  }.flatten!
-  cards.map! {|c| {'id' => c.id, 'json' => c.to_json } }
-
-  return cards, actions
+  [cards, actions]
 end
 
+def filter_to_new_cards(cards)
+  cards.reject { |c| existing_record_ids(table: 'cards').include?(c['id']) }
+end
+
+def filter_to_new_actions(actions)
+  actions.reject { |a| existing_record_ids(table: 'actions').include?(a['id']) }
+end
+
+def existing_record_count(table:)
+  existing_record_ids(table: table).size
+end
+
+# rubocop:disable Metrics/LineLength
 def main
   cards, actions = cards_and_actions
 
-  puts "[info] There are #{existing_record_ids(table: 'cards').size} existing card records"
-  new_cards = cards.select {|c| !existing_record_ids(table: 'cards').include?(c['id']) }
+  puts "[info] There are #{existing_record_count(table: 'cards')} existing card records"
+  new_cards = filter_to_new_cards(cards)
   puts "[info] There are #{new_cards.size} new card records"
-  ScraperWiki.save_sqlite(%w(id), new_cards, 'cards')
+  ScraperWiki.save_sqlite(%w[id], new_cards, 'cards')
 
-  puts "[info] There are #{existing_record_ids(table: 'actions').size} existing action records"
-  new_actions = actions.select {|a| !existing_record_ids(table: 'actions').include?(a['id']) }
+  puts "[info] There are #{existing_record_count(table: 'actions')} existing action records"
+  new_actions = filter_to_new_actions(actions)
   puts "[info] There are #{new_actions.size} new actions records"
-  ScraperWiki.save_sqlite(%w(id), new_actions, 'actions')
+  ScraperWiki.save_sqlite(%w[id], new_actions, 'actions')
 end
+# rubocop:enable Metrics/LineLength
 
-main()
+main if $PROGRAM_NAME == __FILE__
