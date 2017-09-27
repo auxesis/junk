3064d114b2f255cc0f354e285045d853373b8a3e
Lindsay Holmwood <lindsay@holmwood.id.au>
2017-09-27 18:42:53 +1000

    Make it simpler to look up attributes

--- archive/lib/data.rb
+++ archive/lib/data.rb
@@ -17,13 +17,34 @@ class Card < Sequel::Model
   set_primary_key :id
   many_to_one :list
   one_to_many :actions
+
+  plugin :serialization, :json, :json
+  alias attrs json
+  def method_missing(method_name)
+    attrs[method_name.to_s] || attrs[method_name] || super
+  end
+
+  def respond_to_missing?(method_name)
+    attrs[method_name.to_s] || attrs[method_name]
+  end
 end
 
-# Card actions
+# Trello card actions
 class Action < Sequel::Model
   set_primary_key :id
   many_to_one :card
 
+  def date
+    Time.parse(attrs['date']) if attrs['date']
+  end
+
   plugin :serialization, :json, :json
   alias attrs json
+  def method_missing(method_name)
+    attrs[method_name.to_s] || attrs[method_name] || super
+  end
+
+  def respond_to_missing?(method_name)
+    attrs[method_name.to_s] || attrs[method_name]
+  end
 end
