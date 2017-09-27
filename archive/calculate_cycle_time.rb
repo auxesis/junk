1052f4cd285e4708793e2e34f48d5e6eebf424ca
Lindsay Holmwood <lindsay@holmwood.id.au>
2017-09-27 19:22:43 +1000

    Refactor to improve readability and maintainability

--- archive/calculate_cycle_time.rb
+++ archive/calculate_cycle_time.rb
@@ -11,33 +11,55 @@ def weekends?(range)
   range.reject { |d| (1..5).cover?(d.wday) }.size.positive?
 end
 
-lists = {
-  start: List.find(name: 'Ready'),
-  finish: List.find(name: 'DONE – Sprint 5')
-}
+def lists
+  {
+    start: List.find(name: 'Ready'),
+    finish: List.find(name: 'DONE – Sprint 4')
+  }
+end
 
-cards = lists[:finish].cards
-cards.select! { |c| c.attrs['name'] =~ /^\[[S|M|L|\d*XL]\]/ }
-cards.each do |card|
+def extract_card_estimates(card)
   stream = card.labels.map { |l| l['name'] }.find { |name| name =~ /stream/i }
   estimate, title = card.name.split(' ', 2)
   estimate = estimate[1..-2]
+  [stream, estimate, title]
+end
 
-  actions = card.actions.select { |a| a.type == 'updateCard' }
-
-  cycle_start = actions.select do |a|
+def find_cycle_start(actions)
+  actions.select do |a|
     a.data.dig('listBefore', 'id') == lists[:start].id
   end.sort_by(&:date).last
-  cycle_end = actions.select do |a|
-    a.data.dig('listAfter', 'id')  == lists[:finish].id
+end
+
+def find_cycle_end(actions)
+  actions.select do |a|
+    a.data.dig('listAfter', 'id') == lists[:finish].id
   end.sort_by(&:date).first
+end
 
-  entry = [card.id, stream, estimate, title]
-
-  next unless cycle_start && cycle_end # skip cards created in Doing and Review
-
+def print_cycle_time(record:, cycle_start:, cycle_end:)
+  return unless cycle_start && cycle_end
   cycle_seconds = cycle_end.date - cycle_start.date
   cycle_seconds -= 2.days.to_i if weekends?(cycle_start.date..cycle_end.date)
-  entry << cycle_seconds
-  puts entry.join("\t")
+  record << cycle_seconds
+  puts record.join("\t")
 end
+
+def calculate_and_print_cycle_time(card)
+  stream, estimate, title = extract_card_estimates(card)
+
+  actions = card.actions.select { |a| a.type == 'updateCard' }
+  cycle_start = find_cycle_start(actions)
+  cycle_end = find_cycle_end(actions)
+
+  record = [card.id, stream, estimate, title]
+  # skip cards created in Doing and Review
+  print_cycle_time(record: record, cycle_start: cycle_start, cycle_end: cycle_end)
+end
+
+def main
+  cards = lists[:finish].cards.select { |c| c.attrs['name'] =~ /^\[[S|M|L|\d*XL]\]/ }
+  cards.each { |card| calculate_and_print_cycle_time(card) }
+end
+
+main if $PROGRAM_NAME == __FILE__
