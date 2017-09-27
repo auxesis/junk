baf21ad23cc73cf106be0d95c4ebc1f62f42589f
Lindsay Holmwood <lindsay@holmwood.id.au>
2017-09-27 18:13:56 +1000

    Add model for Trello lists

--- archive/lib/data.rb
+++ archive/lib/data.rb
@@ -6,9 +6,16 @@ require 'sequel'
 
 DB = Sequel.sqlite('data.sqlite')
 
+# Trello lists
+class List < Sequel::Model
+  set_primary_key :id
+  one_to_many :cards
+end
+
 # Trello cards
 class Card < Sequel::Model
   set_primary_key :id
+  many_to_one :list
   one_to_many :actions
 end
 
