cba0ca8bd79165b33fcb539c930d5db96f8cee4c
Lindsay Holmwood <lindsay@holmwood.id.au>
2017-11-07 23:21:36 +1100

    Remove scraping code. Build report from scraped data

--- mirroring/pr_stats.rb
+++ mirroring/pr_stats.rb
@@ -1,29 +1,27 @@
 # frozen_string_literal: true
 
-require 'octokit'
+require 'scraperwiki'
+require 'json'
 require 'pry'
-require 'vcr'
-require 'webmock'
 require 'dotenv'
 Dotenv.load
 
-VCR.configure do |config|
-  config.cassette_library_dir = 'fixtures'
-  config.hook_into :webmock
+def target_streams
+  if ENV['TARGET_STREAMS']
+    ENV['TARGET_STREAMS'].split(',').map(&:to_i)
+  else
+    puts 'ERROR: The TARGET_STREAMS environment variable must be set'
+    exit(1)
+  end
 end
 
-Octokit.auto_paginate = true
-
-def repo
-  'envato/elements-backend'
-end
-
-def short_repo
-  repo.split('/').last
-end
-
-def client
-  @client ||= Octokit::Client.new(access_token: ENV['GITHUB_TOKEN'])
+def target_repos
+  if ENV['TARGET_REPOS']
+    ENV['TARGET_REPOS'].split(',')
+  else
+    puts 'ERROR: The TARGET_REPOS environment variable must be set'
+    exit(1)
+  end
 end
 
 def usernames
@@ -41,83 +39,63 @@ def usernames
   end
 end
 
-# rubocop:disable Security/MarshalLoad
-def prs
-  if ENV['FAST']
-    records = Marshal.load(File.read("marshal-#{short_repo}.bin"))
-  else
-    VCR.use_cassette("#{short_repo}-pull-requests", record: :new_episodes) do
-      all_pull_requests = client.pull_requests(repo, state: 'closed')
-      pull_requests = all_pull_requests.select { |pr| pr[:created_at] > Date.parse('2017-02-13').to_time }
-      records = pull_requests.map do |pr|
-        STDERR.puts "Fetching activity for #{repo}##{pr[:number]}"
-        [
-          pr,
-          client.issue_comments(repo, pr[:number]),
-          client.pull_request_comments(repo, pr[:number]),
-          client.pull_request_reviews(repo, pr[:number])
-        ]
-      end
-    end
+def pull_requests_participation(repo:)
+  pull_requests = ScraperWiki.select("* FROM pull_requests WHERE repo = '#{repo}'")
+  pull_requests.map do |pr|
+    pr = JSON.parse(pr['json'])
+    owner = pr['user']['login']
+    number = pr['number']
 
-    File.open("marshal-#{short_repo}.bin", 'w') { |f| f << Marshal.dump(records) }
-  end
-
-  records
-end
-# rubocop:enable Security/MarshalLoad
-
-def contributions
-  prs.map do |pr, issue_comments, pr_comments, pr_reviews|
-    owner = pr[:user][:login]
-    participants = [
-      issue_comments.map { |c| c[:user][:login] },
-      pr_comments.map { |c| c[:user][:login] },
-      pr_reviews.map { |c| c[:user][:login] }
-    ].flatten.uniq - [owner]
+    comments = ScraperWiki.select("* FROM comments WHERE repo = '#{repo}' and pr_id = #{number}")
+    participants = comments.map do |c|
+      JSON.parse(c['json'])['user']['login']
+    end.flatten.uniq - [owner]
 
     {
       repo: repo,
-      number: pr[:number],
+      number: number,
       owner: owner,
       participants: participants,
-      week_of_year: pr[:closed_at].strftime('%W').to_i
+      week_of_year: Time.parse(pr['closed_at']).strftime('%W').to_i
     }
   end
-end
-
-def stream_prs(stream:)
-  contributions.select { |contrib| usernames[contrib[:owner]] == stream }
 end
 
 def add_missing_weeks!(prs_by_week)
   min = prs_by_week.keys.sort.first
   max = prs_by_week.keys.sort.last
-  missing_weeks = (min..max).to_a - prs_by_week.keys
-  missing_weeks.each { |week| prs_by_week[week] = [] }
+  (min..max).to_a.each { |week| prs_by_week[week] ||= [] }
 end
 
-def weekly_pr_counts(stream:nil, repos: [])
-  target_streams = stream ? [stream] : streams
+def weekly_pr_counts(streams:, repos:)
+  output = streams.map do |stream|
+    repos.map do |repo|
+      participations = pull_requests_participation(repo: repo)
+      prs_by_week = participations.select do |part|
+        usernames[part[:owner]] == stream
+      end.group_by do |part|
+        part[:week_of_year]
+      end
 
-  output = target_streams.map do |stream|
-    prs_by_week = stream_prs(stream: stream).group_by { |c| c[:week_of_year] }
-    add_missing_weeks!(prs_by_week)
+      add_missing_weeks!(prs_by_week)
 
-    prs_by_week.map do |week, contribs|
-      logins = contribs.map { |c| c[:participants].uniq }.flatten.map { |u| usernames[u] }
+      prs_by_week.map do |week, parts|
+        logins = parts.map { |c| c[:participants].uniq }.flatten.map { |u| usernames[u] }
 
-      counts = logins.inject(0 => 0, 1 => 0, -1 => 0) do |summary, stream|
-        summary[stream] += 1
-        summary
-      end.values
+        counts = logins.inject(0 => 0, 1 => 0, -1 => 0) do |summary, stream|
+          summary[stream] += 1
+          summary
+        end.values
 
-      [week, stream, counts].flatten
+        [week, stream, counts].flatten
+      end
     end
   end
 
-  output.flatten(1).sort
+  output.flatten(2).sort
 end
 
-report = weekly_pr_counts(stream: nil)
-puts report.map { |c| c.join("\t") }
+if $PROGRAM_NAME == __FILE__
+  report = weekly_pr_counts(streams: target_streams, repos: target_repos)
+  puts report.map { |c| c.join("\t") }
+end
