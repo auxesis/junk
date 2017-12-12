545f6ad726c64fe102c5c740823a286343857d8d
Lindsay Holmwood <lindsay@holmwood.id.au>
2017-12-12 11:24:43 +1100

    First pass at a Slackbot

--- lateral/lateral.rb
+++ lateral/lateral.rb
@@ -6,6 +6,7 @@ require 'dotenv'
 require 'tree'
 require 'pry'
 require 'fuzzy_match'
+require 'slack-ruby-client'
 require 'json'
 
 Dotenv.load
@@ -33,21 +34,21 @@ def add_to_directory(attrs)
   return name
 end
 
+def build_node(attrs)
+  name = attrs['name'].strip
+  add_to_directory(attrs)
+  node = Node.new(name)
+  build_tree(node, attrs['children'])
+  node
+end
+
 def build_tree(parent, children)
-  children.each do |attrs|
-    name = attrs['name'].strip
-    add_to_directory(attrs)
-    node = Node.new(name)
-    build_tree(node, attrs['children'])
-    parent << node
-  end
+  children.each { |attrs| parent << build_node(attrs) }
 end
 
 def dag
   return @root if @root
-  @root = Node.new(json['name'])
-  add_to_directory(json)
-  build_tree(@root,json['children'])
+  @root = build_node(json)
   return @root
 end
 
@@ -64,9 +65,9 @@ def find_in_tree(name)
 end
 
 def parentage(name)
-  find_in_tree(name).parentage&.map(&:name).reverse.map { |n|
+  find_in_tree(name).parentage&.map(&:name)&.reverse.map { |n|
     [n, directory[n]]
-  } << [ name, directory[name] ]
+  }
 end
 
 def children(name)
@@ -79,4 +80,43 @@ def lookup(name)
   index.find_all(name, threshold: 0.5).map { |n| [ n, directory[n] ] }
 end
 
-binding.pry
+def format_name(name, attrs)
+  base = [ name, attrs[:job_title] ].join(' – ')
+  base += ' :small_blue_diamond:' if attrs[:direct_reports]
+  return base
+end
+
+Slack.configure do |config|
+  config.token = ENV['SLACK_API_TOKEN']
+end
+
+client = Slack::RealTime::Client.new
+
+client.on :message do |data|
+  client.typing channel: data.channel
+  case data.text
+  when /^find (?<name>.+)$/i
+    name = $~[:name]
+    text = lookup(name).map { |name, attrs|
+      [ name, attrs[:job_title] ].join(' – ')
+    }.join("\n")
+    client.message(channel: data.channel, text: text)
+  when /^chart (?<name>.+)$/i
+    name = lookup($~[:name]).first.first
+    text = []
+    text << parentage(name).map { |name, attrs| ':arrow_up: ' + format_name(name, attrs) }
+    text << ':star: ' + format_name(name, directory[name])
+    text << children(name).map { |name, attrs| ':arrow_right_hook: ' + format_name(name, attrs) }
+    client.message(channel: data.channel, text: text.join("\n"))
+  when /^debug.+/i
+    p data
+    client.message(channel: data.channel, text: data.inspect)
+  end
+end
+
+def main
+  client.start!
+end
+
+main if $PROGRAM_NAME == __FILE__
+
