3981d84da054b5304cc8c84583edaa287fbaafd1
Lindsay Holmwood <lindsay@holmwood.id.au>
2018-05-22 14:59:45 +1000

    De-lint

--- groupmap/extract_actions_from_groupmap_page_source.rb
+++ groupmap/extract_actions_from_groupmap_page_source.rb
@@ -1,15 +1,17 @@
-require 'json'
-require 'uri'
+# frozen_string_literal: true
 
-if ARGV.first == nil || ARGV.first == '-'
-  lines = STDIN.readlines
-else
-  lines = File.readlines(ARGV.first)
-end
+require 'json'
+require 'cgi'
+
+lines = if ARGV.first.nil? || ARGV.first == '-'
+          STDIN.readlines
+        else
+          File.readlines(ARGV.first)
+        end
 
 target_line = lines.grep(/window\["SNAPSHOT"\]/).first
-escaped_json = target_line[/JSON.parse\(unescape\("(.*)"/,1]
-json = URI.decode(escaped_json)
+escaped_json = target_line[/JSON.parse\(unescape\("(.*)"/, 1]
+json = CGI.unescape(escaped_json)
 data = JSON.parse(json)
 
 data['Action'].each do |action|
