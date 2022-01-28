430ad324a395772443087948da66923c75a92adb
Lindsay Holmwood <lindsay@holmwood.id.au>
2022-01-28 11:21:57 +1100

    Be more liberal about what we consider to be 'low stock'

--- ikea_kitchen/find_urgent_items.rb
+++ ikea_kitchen/find_urgent_items.rb
@@ -27,7 +27,7 @@ def main
   puts "Low stock for the following items:\n\n"
   items.each do |item|
     total = by_sku[item[:sku]].map { |stock| stock["quantity"] }.sum
-    if (1..5).include?(total)
+    if (1..10).include?(total)
       available_stores = by_sku[item[:sku]].select { |s| s["quantity"] > 0 }.map { |s| s["store"] }
       puts "#{ikeaify_sku(item[:sku])} (#{item[:name]}): want #{item[:quantity]} but only #{total} available (at #{available_stores.join(", ")})"
     end
