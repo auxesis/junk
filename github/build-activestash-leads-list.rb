e2bbdc03b40d77f75891edc7b9651c86a2565d57
Lindsay Holmwood <lindsay@holmwood.id.au>
2022-06-27 23:32:47 +1000

    Add scraper for ActiveStash leads

---
+++ github/build-activestash-leads-list.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/env ruby
+
+require "scraperwiki"
+require "pry"
+require "netrc"
+require "octokit"
+
+Octokit.auto_paginate = true
+client = Octokit::Client.new(netrc: true)
+client.login
+
+search = "language:ruby location:australia type:user"
+
+unless search
+  puts "Usage: #{$PROGRAM_NAME} '<user search query>'"
+  exit(1)
+end
+
+user_results = client.search_users(search)
+
+user_results.items.each_with_index do |result, index|
+  puts "Scraping metadata for: #{result.login}"
+  user = client.user(result.login).to_hash
+  user.select { |k, v| !!v == v }.each { |k, v| user[k] = v.to_s }
+  ScraperWiki.save_sqlite(%i[id], user, "users")
+end
